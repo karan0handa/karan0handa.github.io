ca9cbb775f6f6bf3cd0a4c623aa3b3c1f838b07b
Karan Handa <karan.handa.fox@gmail.com>
2024-01-01 21:30:48 +0530

    chore: add cube animation and fix some styling

---
+++ src/utils/animation.ts
@@ -0,0 +1,57 @@
+import Zdog from 'zdog';
+
+console.log('yo')
+
+let illo = new Zdog.Illustration({
+    element: '.zdog-canvas',
+    rotate: { x: -Zdog.TAU / 10, y: Zdog.TAU / 8 },
+    dragRotate: true,
+})
+
+console.log(illo)
+
+let smallGroup = new Zdog.Group({ addTo: illo })
+let bigGroup = new Zdog.Group({ addTo: illo })
+
+let smallCube = new Zdog.Box({
+    addTo: smallGroup,
+    width: 60,
+    height: 60,
+    depth: 60,
+    stroke: false,
+    fill: true,
+    color: '#175d96'
+})
+smallCube.copy({
+    addTo: smallGroup,
+    stroke: 1,
+    fill: false,
+    color: '#fff'
+})
+
+let bigCube = new Zdog.Box({
+    addTo: illo,
+    width: 120,
+    height: 120,
+    depth: 120,
+    stroke: false,
+    fill: true,
+    color: 'rgba(141, 214, 249, 0.5)'
+})
+bigCube.copy({
+    addTo: bigCube,
+    stroke: 3,
+    fill: false,
+    color: '#FFF'
+})
+
+function animate() {
+    smallGroup.rotate.y += 0.01
+    bigCube.rotate.y -= 0.01
+    // Fun rotation
+    // illo.rotate.z += 0.01
+    illo.updateRenderGraph()
+    requestAnimationFrame(animate)
+}
+
+animate()
